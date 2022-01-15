42c0c45c22ec381e31abe51a44e206fcd552c6e9
Pekka Turtola <pekka.turtola@mavericks.fi>
2022-01-15 19:39:37 +0200

    Start from row index 1

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,18 +1,28 @@
 export class Board {
   width;
   height;
+  matrix;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.matrix = Array(this.height)
+      .fill(0)
+      .map(() => Array(this.width).fill("."));
+  }
+
+  drop(block) {
+    this.matrix[0][1] = block;
   }
 
   toString() {
-    const row = Array(this.width).fill(".");
     return (
-      Array(this.height)
-        .fill(0)
-        .map(() => row.join(""))
+      this.matrix
+        .map((row) =>
+          row
+            .map((cell) => (typeof cell === "string" ? cell : cell.color))
+            .join("")
+        )
         .join("\n") + "\n"
     );
   }
