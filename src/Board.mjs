d5f59341300ded79af878a313887c9f807088e0d
Pekka Turtola <pekka.turtola@mavericks.fi>
2022-01-15 19:59:52 +0200

    One tick at the time

--- src/Board.mjs
+++ src/Board.mjs
@@ -2,17 +2,30 @@ export class Board {
   width;
   height;
   matrix;
+  dropX;
+  dropY;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.matrix = Array(this.height)
       .fill(0)
-      .map(() => Array(this.width).fill("."));
+      .map(() => Array(this.width).fill(0));
   }
 
   drop(block) {
-    this.matrix[0][1] = block;
+    if (typeof this.dropY === "number") {
+      throw new Error("already falling");
+    }
+    this.dropX = Math.floor(this.width / 2);
+    this.dropY = 0;
+    this.matrix[this.dropY][this.dropX] = block;
+  }
+
+  tick() {
+    this.matrix[this.dropY + 1][this.dropX] =
+      this.matrix[this.dropY][this.dropX];
+    this.matrix[this.dropY][this.dropX] = 0;
   }
 
   toString() {
@@ -20,7 +33,7 @@ export class Board {
       this.matrix
         .map((row) =>
           row
-            .map((cell) => (typeof cell === "string" ? cell : cell.color))
+            .map((cell) => (typeof cell === "number" ? "." : cell.color))
             .join("")
         )
         .join("\n") + "\n"
