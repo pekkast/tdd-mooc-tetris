7dbfaf3725eb04305cf07197175f36fc7e9c9a70
Pekka Turtola <pekka.turtola@mavericks.fi>
2022-01-23 22:40:09 +0200

    Fix bad testing structure

--- test/testing.test.mjs
+++ test/testing.test.mjs
@@ -1,10 +1,26 @@
 import { expect } from "chai";
 import { normalize } from "./testing.mjs";
 
-it("normalize", () => {
-  expect(normalize("")).to.equal("\n");
-  expect(normalize("  x  ")).to.equal("x\n");
-  expect(normalize("   x\n   x")).to.equal("x\nx\n");
-  expect(normalize("   x\n   x\n")).to.equal("x\nx\n");
-  expect(normalize("\n   x\n   x")).to.equal("x\nx\n");
+describe("testing", () => {
+  describe("normalize", () => {
+    it("adds line end", () => {
+      expect(normalize("")).to.equal("\n");
+    });
+
+    it("removes whitespace from start and end", () => {
+      expect(normalize("  x  ")).to.equal("x\n");
+    });
+
+    it("handles line ends in middle of string", () => {
+      expect(normalize("   x\n   x")).to.equal("x\nx\n");
+    });
+
+    it("does not add additional line ending", () => {
+      expect(normalize("   x\n   x\n")).to.equal("x\nx\n");
+    });
+
+    it("removes line ending from start", () => {
+      expect(normalize("\n   x\n   x")).to.equal("x\nx\n");
+    });
+  });
 });
